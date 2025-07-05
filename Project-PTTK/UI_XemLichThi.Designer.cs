8fbde44f47431d7d7086208dd115cde845a3a976
quocviett0123 <22120432@student.hcmus.edu.vn>
2025-07-05 16:35:54 +0700

    ui

---
+++ Project-PTTK/UI_XemLichThi.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Project_PTTK
+{
+    partial class UI_XemLichThi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(77, 117);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(758, 99);
+            panel1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(209, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(292, 39);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(203, 32);
+            label2.TabIndex = 0;
+            label2.Text = "Tìm kiếm dịch vụ:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            dataGridView1.Location = new Point(77, 222);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.Size = new Size(1030, 478);
+            dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.Location = new Point(366, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(453, 65);
+            label1.TabIndex = 3;
+            label1.Text = "LỊCH THI ĐANG MỞ";
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Dịch vụ";
+            Column1.MinimumWidth = 10;
+            Column1.Name = "Column1";
+            Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Ngày thi";
+            Column2.MinimumWidth = 10;
+            Column2.Name = "Column2";
+            Column2.Width = 200;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Giờ thi";
+            Column3.MinimumWidth = 10;
+            Column3.Name = "Column3";
+            Column3.Width = 200;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Phòng thi";
+            Column4.MinimumWidth = 10;
+            Column4.Name = "Column4";
+            Column4.Width = 200;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Còn trống";
+            Column5.MinimumWidth = 10;
+            Column5.Name = "Column5";
+            Column5.Width = 200;
+            // 
+            // UI_XemLichThi
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1184, 733);
+            Controls.Add(panel1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "UI_XemLichThi";
+            Text = "LỊCH THI HIỆN CÓ CỦA TRUNG TÂM";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+    }
+}
