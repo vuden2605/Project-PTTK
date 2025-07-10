c4c2d9c347d31837328d5b6c4b6bb798e3173f6a
Vu Le Duy <vuleduy@outlook.com>
2025-07-10 16:45:25 +0700

    Added PhongThi, LichThi class

--- Project-PTTK/Model/DichVu.cs
+++ Project-PTTK/Model/DichVu.cs
@@ -12,4 +12,21 @@ namespace Project_PTTK.Model
         public string TenDichVu { get; set; } = null!;
         public decimal Gia { get; set; }
     }
+
+    public class PhongThi
+    {
+        public int MaPhongThi { get; set; }
+        public string TenPhong { get; set; } = null!;
+    }
+    public class LichThi
+    {
+        public int MaLichThi { get; set; }
+        public DateOnly NgayThi { get; set; }
+        public TimeOnly GioThi { get; set; }
+        public int SoThiSinhToiDa { get; set; }
+        public int SoThiSinhDaDangKy { get; set; }
+        public int MaPhongThi { get; set; }
+        public int MaDichVu { get; set; }
+        public int NhanVienLap { get; set; }
+    }
 }
