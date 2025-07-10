147cdbe81f2fcee6564ac061bc18243f51d6b2af
Vu Le Duy <vuleduy@outlook.com>
2025-07-10 16:33:34 +0700

    Added NhanVien class

---
+++ Project-PTTK/Model/NhanVien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PTTK.Model
+{
+    public class NhanVien
+    {
+        public int MaNV { get; set; }
+        public string MatKhau { get; set; } = null!;
+        public string HoTen { get; set; } = null!;
+        public string SDT { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string VaiTro { get; set; } = null!;
+        public decimal Luong { get; set; }
+    }
+}
