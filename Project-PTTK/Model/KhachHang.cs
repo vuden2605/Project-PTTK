215ee3ae4a9f0d26e073def13bfc76f59706a125
Vu Le Duy <vuleduy@outlook.com>
2025-07-10 16:24:19 +0700

    Added model KhachHang and subclasses

---
+++ Project-PTTK/Model/KhachHang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PTTK.Model
+{
+    public class KhachHang()
+    {
+        public int MaKH { get; set; }
+        public string LoaiKhachHang { get; set; } = null!;
+        public required string Email { get; set; } = null!;
+    }
+
+    public class KhachHangTuDo : KhachHang
+    {
+        public string Ten { get; set; } = null!;
+        public DateTime NgaySinh { get; set; }
+    }
+
+    public class KhachHangDonVi: KhachHang
+    {
+        public string TenDichVu { get; set; } = null!;
+        public string DiaChi { get; set; } = null!;
+    }
+}
