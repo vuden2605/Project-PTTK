0679fa55e517654e61ee53074b5373bebe228f1a
Vu Le Duy <vuleduy@outlook.com>
2025-07-10 21:40:59 +0700

    Separated LichThi class

--- Project-PTTK/Model/DichVu.cs
+++ Project-PTTK/Model/DichVu.cs
@@ -18,15 +18,4 @@ namespace Project_PTTK.Model
         public int MaPhongThi { get; set; }
         public string TenPhong { get; set; } = null!;
     }
-    public class LichThi
-    {
-        public int MaLichThi { get; set; }
-        public DateOnly NgayThi { get; set; }
-        public TimeOnly GioThi { get; set; }
-        public int SoThiSinhToiDa { get; set; }
-        public int SoThiSinhDaDangKy { get; set; }
-        public int MaPhongThi { get; set; }
-        public int MaDichVu { get; set; }
-        public int NhanVienLap { get; set; }
-    }
 }
