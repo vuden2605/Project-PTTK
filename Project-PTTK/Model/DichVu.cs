6e1e25cda50debe319e64e14dee238e16b011e4e
Vu Le Duy <vuleduy@outlook.com>
2025-07-10 16:38:00 +0700

    Added DichVu class

---
+++ Project-PTTK/Model/DichVu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PTTK.Model
+{
+    public class DichVu
+    {
+        public int MaDichVu { get; set; }
+        public string TenDichVu { get; set; } = null!;
+        public decimal Gia { get; set; }
+    }
+}
