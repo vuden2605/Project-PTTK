5d96cb8e8ed68f5e893c11ddaf4f2f4a2efe860f
Vu Le Duy <vuleduy@outlook.com>
2025-07-11 20:05:58 +0700

    renamed namespace

--- Project-PTTK/DataAccess/PhieuDAO.cs
+++ Project-PTTK/DataAccess/PhieuDAO.cs
@@ -8,7 +8,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq.Expressions;
 
-namespace Project_PTTK.DataAccess
+namespace Project_PTTK.DataAccess.Phieu
 {
     public class ChiTietPhieuDangKyDAO
     {
