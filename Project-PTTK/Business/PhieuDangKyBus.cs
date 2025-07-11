688c6e0525f3b937e56fccc9e57ff855ed5ae398
vuden2605 <vuden2605@gmail.com>
2025-07-11 22:20:25 +0700

    up

--- Project-PTTK/Business/PhieuDangKyBus.cs
+++ Project-PTTK/Business/PhieuDangKyBus.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Project_PTTK.Model;
+using Project_PTTK.DataAccess.Phieu;
 
 namespace Project_PTTK.Business
 {
-    class PhieuDangKyBus
+    public class PhieuDangKyBUS
     {
+        private readonly PhieuDangKyDAO _phieuDangKyDAO;
+        public PhieuDangKyBUS(PhieuDangKyDAO phieuDangKyDAO)
+        {
+            _phieuDangKyDAO = phieuDangKyDAO;
+        }
+        public List<PhieuDangKy> LayDanhSach() => _phieuDangKyDAO.LayDanhSach();
+        public PhieuDangKy? LayTheoMa(int maPhieuDangKy) => _phieuDangKyDAO.LayTheoMa(maPhieuDangKy);
     }
 }
