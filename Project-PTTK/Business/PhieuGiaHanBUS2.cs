49f98bd994c8fe04526858b6207b21a8973669cd
Vu Le Duy <vuleduy@outlook.com>
2025-07-11 20:05:34 +0700

    added

---
+++ Project-PTTK/Business/PhieuGiaHanBUS2.cs
@@ -0,0 +1,25 @@
+﻿using Project_PTTK.DataAccess.Phieu;
+using Project_PTTK.Model;
+
+namespace Project_PTTK.Business
+{
+    public class PhieuGiaHanBUS2
+    {
+        private readonly PhieuGiaHanDAO phieuGiaHanDAO = new PhieuGiaHanDAO();
+        public List<PhieuGiaHan> LayDanhSach()
+        {
+            List<PhieuGiaHan> phieuGiaHans = phieuGiaHanDAO.LayDanhSach();
+            return phieuGiaHans;
+        }
+        public PhieuGiaHan? LayTheoMa(int maPhieuGiaHan)
+        {
+            PhieuGiaHan? phieuGiaHan = phieuGiaHanDAO.LayTheoMa(maPhieuGiaHan);
+            return phieuGiaHan;
+        }
+        public void TaoPhieu(PhieuGiaHan phieuGiaHan)
+        {
+            phieuGiaHanDAO.Them(phieuGiaHan);
+        }
+        
+    }
+}
